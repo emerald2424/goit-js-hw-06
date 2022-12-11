bd628aa57aacbc21bd6b2376246087d3da77f9ff
emerald2424 <109916178+emerald2424@users.noreply.github.com>
2022-12-11 11:26:27 +0200

    updates task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,22 +7,16 @@ const ingredients = [
   'Condiments',
 ];
 
-// const list = ingredients.map(ingredient => {
-//   const li = document.createElement('li');
-//   li.textContent = ingredient;
-//   li.classList.add('item');
-  
-//   // console.log(li)
-// }).join('');
-
-// document.querySelector('#ingredients').innerHTML = list;
-
-
-
-ingredients.forEach(ingredient => {
+const list = ingredients.map(ingredient => {
   const li = document.createElement('li');
   li.textContent = ingredient;
   li.classList.add('item');
-
-  document.querySelector('#ingredients').append(li)
+  return li;
+  
 });
+
+document.querySelector('#ingredients').append(...list);
+
+
+
+
